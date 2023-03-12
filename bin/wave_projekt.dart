879e46e5fd7235e7a19a089c4a7eec4ed2ca4b58
Your Name <you@example.com>
2023-03-12 16:01:02 +0500

    hgghj

--- bin/wave_projekt.dart
+++ bin/wave_projekt.dart
@@ -1,5 +1,12 @@
 import 'package:wave_projekt/wave_projekt.dart' as wave_projekt;
 
+/*
+Created by Shamsiddin Mardonov on 15:58:13 12.03.2023
+© 2023 @Shamsiddin_Mardonov 
+*/
+/*
+*Theme:::
+*/
 void main(List<String> arguments) {
   print('Hello world: ${wave_projekt.calculate()}!');
 }
