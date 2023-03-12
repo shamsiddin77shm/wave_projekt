979759b98f02fb786eafd33dc5535eace370be23
Your Name <you@example.com>
2023-03-12 15:47:05 +0500

    first commit

---
+++ bin/wave_projekt.dart
@@ -0,0 +1,5 @@
+import 'package:wave_projekt/wave_projekt.dart' as wave_projekt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wave_projekt.calculate()}!');
+}
---
+++ test/wave_projekt_test.dart
@@ -0,0 +1,8 @@
+import 'package:wave_projekt/wave_projekt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
